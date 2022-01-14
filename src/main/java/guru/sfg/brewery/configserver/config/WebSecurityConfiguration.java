79ef653f0b609f63edd0e24430c6bbabebe4ffbf
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2022-01-14 12:34:46 +0100

    Zipkin con Logback en formato JSON Limpieza de dependencias duplicadas

--- src/main/java/guru/sfg/brewery/configserver/config/WebSecurityConfiguration.java
+++ src/main/java/guru/sfg/brewery/configserver/config/WebSecurityConfiguration.java
@@ -5,12 +5,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Configuración de seguridad
+ */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     /**
-     * Habilitamos la basic auth para poder usar el encrypt y decript
+     * Habilitamos la basic auth para poder usar el encrypt y decript con los valores de configuración que van cifrados
      * @param http
      * @throws Exception
      */
